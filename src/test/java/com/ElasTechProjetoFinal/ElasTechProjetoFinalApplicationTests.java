ef39bc6cc424d339076b25990f1a97fcaa7a31ea
BiancalBarreto <bianca.lb2004@gmail.com>
2024-05-02 22:00:50 -0300

    primeiro commit

---
+++ src/test/java/com/ElasTechProjetoFinal/ElasTechProjetoFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ElasTechProjetoFinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElasTechProjetoFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
