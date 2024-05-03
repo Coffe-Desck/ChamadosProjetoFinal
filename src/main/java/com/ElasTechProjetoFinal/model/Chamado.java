b2aa6659e902886e056b2090f96e4694f315b6d7
BiancalBarreto <bianca.lb2004@gmail.com>
2024-05-03 19:58:07 -0300

    model usuario

--- src/main/java/com/ElasTechProjetoFinal/model/Chamado.java
+++ src/main/java/com/ElasTechProjetoFinal/model/Chamado.java
@@ -1,4 +1,14 @@
 package com.ElasTechProjetoFinal.model;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class Chamado {
+
+    @Id
+    private int id;
+
+
 }
