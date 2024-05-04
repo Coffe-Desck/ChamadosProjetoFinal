875860f02db0bbd4d1d4703a0ca4cae2bae67c4e
Nataly <natalycarvalho404@gmail.com>
2024-05-04 20:51:32 -0300

    Cadastro, alteração, exclusão, findById de usuario

--- src/main/java/com/ElasTechProjetoFinal/controller/UsuarioController.java
+++ src/main/java/com/ElasTechProjetoFinal/controller/UsuarioController.java
@@ -1,4 +1,43 @@
 package com.ElasTechProjetoFinal.controller;
 
+import com.ElasTechProjetoFinal.model.Usuario;
+import com.ElasTechProjetoFinal.model.UsuarioLogin;
+import com.ElasTechProjetoFinal.service.UsuarioService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/usuario")
 public class UsuarioController {
+    @Autowired
+    private UsuarioService usuarioService;
+
+    @PostMapping("/cadastro")
+    public Usuario save(@RequestBody @Valid Usuario usuario) {
+        return this.usuarioService.save(usuario);
+    }
+
+    @PostMapping("/logar")
+    public UsuarioLogin login(@RequestBody @Valid UsuarioLogin usuarioLogin) {
+        UsuarioLogin usuarioAutenticado = usuarioService.autenticarUsuario(usuarioLogin);
+        return usuarioAutenticado;
+    }
+
+    @GetMapping("/{id}")
+    public Usuario findById(@PathVariable Long id) {
+        return this.usuarioService.findById(id);
+    }
+    @DeleteMapping("/deletar/{id}")
+    public Usuario deleteById(@PathVariable Long id) {
+        return this.usuarioService.deleteById(id);
+    }
+
+    @PutMapping("/alterar/{id}")
+    public Usuario updateById(@PathVariable @Valid Long id, @RequestBody Usuario usuario) {
+        return this.usuarioService.updateById(id, usuario);
+    }
+
 }
