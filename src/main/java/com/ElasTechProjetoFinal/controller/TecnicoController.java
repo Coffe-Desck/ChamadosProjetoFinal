f7e69b589d78c066c2eecb594fb59ca37a18c9ae
JulianaCRodrigues <juucrodrigues@gmail.com>
2024-05-09 19:43:39 -0300

    Update tecnico e chamados

--- src/main/java/com/ElasTechProjetoFinal/controller/TecnicoController.java
+++ src/main/java/com/ElasTechProjetoFinal/controller/TecnicoController.java
@@ -6,17 +6,22 @@ import com.ElasTechProjetoFinal.model.Tecnico;
 import com.ElasTechProjetoFinal.model.UsuarioLogin;
 import com.ElasTechProjetoFinal.service.TecnicoService;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RestController
 @RequestMapping("/tecnicos")
+@Tag(name = "Tecnicos")
 public class TecnicoController {
 
-
     private final TecnicoService tecnicoService;
-
     private final ObjectMapper mapper;
 
     public TecnicoController(TecnicoService tecnicoService, ObjectMapper mapper) {
@@ -24,32 +29,57 @@ public class TecnicoController {
         this.mapper = mapper;
     }
 
-    @PostMapping("/cadastro")
-    public Tecnico save(@RequestBody @Valid Tecnico tecnico) {
-        return this.tecnicoService.save(tecnico);
+    @Operation(summary = "Cria um técnico", method = "POST")
+    @PostMapping()
+    public ResponseEntity<Tecnico> save(@RequestBody @Valid Tecnico tecnico) {
+        Tecnico savedTecnico = tecnicoService.save(tecnico);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedTecnico);
     }
 
-    @PostMapping("/logar")
-    public UsuarioLogin login(@RequestBody @Valid UsuarioLogin usuarioLogin) {
+    @Operation(summary = "Realiza o login de um técnico", method = "POST")
+    @PostMapping("/login")
+    public ResponseEntity<UsuarioLogin> login(@RequestBody @Valid UsuarioLogin usuarioLogin) {
         UsuarioLogin usuarioAutenticado = tecnicoService.autenticarUsuario(usuarioLogin);
-        return usuarioAutenticado;
+        return ResponseEntity.ok(usuarioAutenticado);
     }
 
-    @PutMapping("/alterar/{id}")
-    public ResponseEntity<TecnicoResponse> updateById(@PathVariable @Valid Long id, @RequestBody Tecnico tecnico) {
-         tecnicoService.updateById(id, tecnico);
-         TecnicoResponse resposta = new TecnicoResponse();pit
-         return ResponseEntity.accepted().body(resposta);
+    @Operation(summary = "Busca todos os técnicos", method = "GET")
+    @GetMapping
+    public ResponseEntity<List<TecnicoResponse>> findAll() {
+        List<Tecnico> tecnicos = tecnicoService.findAll();
+        if (!tecnicos.isEmpty()) {
+            List<TecnicoResponse> resposta = tecnicos.stream()
+                    .map(tecnico -> mapper.convertValue(tecnico, TecnicoResponse.class))
+                    .collect(Collectors.toList());
+            return ResponseEntity.ok(resposta);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 
+    @Operation(summary = "Atualiza um técnico pelo ID", method = "PUT")
+    @PutMapping("/{id}")
+    public ResponseEntity<TecnicoResponse> updateById(@PathVariable @Valid Long id, @RequestBody @Valid Tecnico tecnico) {
+        Tecnico updatedTecnico = tecnicoService.updateById(id, tecnico);
+        TecnicoResponse resposta = mapper.convertValue(updatedTecnico, TecnicoResponse.class);
+        return ResponseEntity.ok(resposta);
+    }
+
+    @Operation(summary = "Busca um técnico pelo ID", method = "GET")
     @GetMapping("/{id}")
-    public Tecnico findById(@PathVariable @Valid Long id) {
-        return this.tecnicoService.findById(id);
+    public ResponseEntity<Tecnico> findById(@PathVariable @Valid Long id) {
+        Tecnico tecnico = tecnicoService.findById(id);
+        if (tecnico != null) {
+            return ResponseEntity.ok(tecnico);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 
-    @DeleteMapping("/delete/{id}")
+    @Operation(summary = "Deleta um técnico pelo ID", method = "DELETE")
+    @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteById(@PathVariable @Valid Long id) {
-       tecnicoService.deleteById(id);
-       return ResponseEntity.noContent().build();
+        tecnicoService.deleteById(id);
+        return ResponseEntity.noContent().build();
     }
 }
---
+++ src/main/java/com/ElasTechProjetoFinal/model/EnumRole.java
@@ -0,0 +1,25 @@
+package com.ElasTechProjetoFinal.model;
+
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+
+@Getter
+@NoArgsConstructor
+public enum EnumRole implements GrantedAuthority {
+    ADMIN("ROLE_ADMIN"),
+    USER("ROLE_USER"),
+    TECHNICIAN("ROLE_TECHNICIAN");
+
+    private String authority;
+
+    EnumRole(String authority) {
+        this.authority = authority;
+    }
+
+    @Override
+    public String getAuthority() {
+        return authority;
+    }
+}
