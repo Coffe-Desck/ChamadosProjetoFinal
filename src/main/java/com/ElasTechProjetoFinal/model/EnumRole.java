148f3f0f6f24b4b383d67f3967cc00636ee49c5d
JulianaCRodrigues <juucrodrigues@gmail.com>
2024-05-09 20:12:18 -0300

    Resolvendo bugs

--- src/main/java/com/ElasTechProjetoFinal/model/EnumRole.java
+++ src/main/java/com/ElasTechProjetoFinal/model/EnumRole.java
@@ -2,22 +2,22 @@ package com.ElasTechProjetoFinal.model;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
 
-//@Getter
-//@NoArgsConstructor
-//public enum EnumRole implements GrantedAuthority {
-//    ADMIN("ROLE_ADMIN"),
-//    USER("ROLE_USER"),
-//    TECHNICIAN("ROLE_TECHNICIAN");
-//
-//    private String authority;
-//
-//    EnumRole(String authority) {
-//        this.authority = authority;
-//    }
-//
-//    @Override
-//    public String getAuthority() {
-//        return authority;
-//    }
-//}
+@Getter
+@NoArgsConstructor
+public enum EnumRole implements GrantedAuthority {
+   ADMIN("ROLE_ADMIN"),
+    USER("ROLE_USER"),
+    TECHNICIAN("ROLE_TECHNICIAN");
+    private String authority;
+
+    EnumRole(String authority) {
+        this.authority = authority;
+    }
+
+    @Override
+    public String getAuthority() {
+        return authority;
+    }
+}
